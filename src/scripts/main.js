9a97b145e16ca412f784d987e776c1a1951cbc71
Vladyslav Liashenko <vladislav.liashenko1@gmail.com>
2025-12-16 23:15:58 +0200

    solv

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const employees = document.querySelectorAll('li');
+const employeesOfArray = [...employees];
+
+function sortList(list) {
+  return [...list].sort(
+    (a, b) =>
+      +b.dataset.salary.replace(/[$,]/g, '') -
+      +a.dataset.salary.replace(/[$,]/g, ''),
+  );
+}
+
+function getEmployees(list) {
+  const arr = [];
+
+  for (let i = 0; i < list.length; i++) {
+    arr[i] = {
+      name: list[i].textContent.trim(),
+      position: list[i].dataset.position,
+      salary: list[i].dataset.salary,
+      age: list[i].dataset.age,
+    };
+  }
+
+  return arr;
+}
+getEmployees(sortList(employeesOfArray));
